6e6c2b44e20cbb6b59e0dc34d3df62bad4ad3d7b
Tesfamichael Abebe <tesfamichael.abebe-ug@aau.edu.et>
2026-02-14 20:29:47 +0300

    Time: 14 ms (51.59%), Space: 22 MB (56.62%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,13 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagram = defaultdict(list)
+        for word in strs:
+            key = "".join(sorted(word.replace(" "," ")))
+            anagram[key].append(word)
+        return list(anagram.values())
+
+            
+
+            
+
+        
