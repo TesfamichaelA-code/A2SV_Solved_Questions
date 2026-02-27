1553c3773a974029621224a58bbb937a8f36c8c1
Tesfamichael Abebe <doctorengineertesfamichael@gmail.com>
2026-02-27 17:32:36 +0300

    codeforces: solved beautiful matrix problem

---
+++ BeautifulMatrix.py
@@ -0,0 +1,11 @@
+import sys
+ 
+def solve():
+    for i in range(5):
+        row = list(map(int, sys.stdin.readline().split()))
+        if 1 in row:
+            print(abs(i - 2) + abs(row.index(1) - 2))
+            return
+ 
+if __name__ == "__main__":
+    solve()
