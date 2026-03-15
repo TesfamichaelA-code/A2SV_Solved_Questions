fb1b946828c2b3cd54f39031cc61fc55cb8c737f
Tesfamichael Abebe <tesfamichael.abebe-ug@aau.edu.et>
2026-03-15 19:04:30 +0300

    Add solution for Array Splitting

---
+++ codeforces/1197C-Array-Splitting/1197C-Array-Splitting.py
@@ -0,0 +1,28 @@
+import sys
+def solve():
+    data = sys.stdin.read().split()
+    if not data:
+        return
+    
+    n = int(data[0])
+    k = int(data[1])
+    a = [int(x) for x in data[2:]]
+    
+    if k == 1:
+        print(a[n-1] - a[0])
+        return
+ 
+    diffs = []
+    for i in range(n - 1):
+        diffs.append(a[i+1] - a[i])
+    
+    diffs.sort(reverse=True)
+    ans = a[n-1] - a[0]
+    
+    for i in range(k - 1):
+        ans -= diffs[i]
+        
+    print(ans)
+ 
+if __name__ == "__main__":
+    solve()
