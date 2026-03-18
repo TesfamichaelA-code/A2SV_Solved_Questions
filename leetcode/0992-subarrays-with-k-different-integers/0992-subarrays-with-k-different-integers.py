bc184e577f9acba8a71024bc939260eeb16a97fa
Tesfamichael Abebe <tesfamichael.abebe-ug@aau.edu.et>
2026-03-18 19:02:11 +0300

    Add solution for Subarrays with K Different Integers

---
+++ leetcode/0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.py
@@ -0,0 +1,20 @@
+class Solution:
+    def subarraysWithKDistinct(self, nums: List[int], k: int) -> int:
+        def atMost(k):
+            count = defaultdict(int)
+            left = 0
+            res = 0
+            for right in range(len(nums)):
+                count[nums[right]] += 1
+                while len(count) > k:
+                    count[nums[left]] -= 1
+                    if count[nums[left]] == 0:
+                        del count[nums[left]]
+                    left += 1
+                res += right - left + 1
+            
+            return res
+        
+        return atMost(k) - atMost(k - 1)
+        
+        
