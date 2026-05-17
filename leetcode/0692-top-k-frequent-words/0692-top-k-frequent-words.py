b77f6867240bb23858213606acf5557df827962e
Tesfamichael Abebe <tesfamichael.abebe-ug@aau.edu.et>
2026-05-17 22:02:26 +0300

    Add solution for Top K Frequent Words

---
+++ leetcode/0692-top-k-frequent-words/0692-top-k-frequent-words.py
@@ -0,0 +1,5 @@
+class Solution:
+    def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        count = Counter(words)
+
+        return sorted(count.keys(), key=lambda word: (-count[word], word))[:k]
